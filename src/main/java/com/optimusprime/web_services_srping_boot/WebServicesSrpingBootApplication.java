9dc41508e061baae2fca2e3c61c4ade277eb67f2
Menik <menikdisney@gmail.com>
2018-12-14 09:40:27 +0600

    Initial commit

---
+++ src/main/java/com/optimusprime/web_services_srping_boot/WebServicesSrpingBootApplication.java
@@ -0,0 +1,14 @@
+package com.optimusprime.web_services_srping_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesSrpingBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesSrpingBootApplication.class, args);
+	}
+
+}
+
